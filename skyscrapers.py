cf6ff40676f549d0a2ba8b2d4d2d8af9d68df5b8
Vlada04 <v.omelchenko@ucu.edu.ua>
2021-02-09 20:05:30 +0200

    Add documentation

--- skyscrapers.py
+++ skyscrapers.py
@@ -3,6 +3,7 @@ def read_input(path: str):
     Read game board file from path.
     Return list of str.
 
+
     >>> read_input("check.txt")
     ['***21**', '452453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']
     """
